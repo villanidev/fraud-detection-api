cc316d8414bfc133218a933303811c2b7588cf4e
Thiago Villani <villani.thiago01@gmail.com>
2026-05-22 18:46:42 +0200

    cache respostas

--- src/main/java/villani/dev/fraud/DecisionResponse.java
+++ src/main/java/villani/dev/fraud/DecisionResponse.java
@@ -1,3 +1,22 @@
 package villani.dev.fraud;
 
-public record DecisionResponse(boolean approved, double score) {}
+import java.nio.charset.StandardCharsets;
+
+public class DecisionResponse {
+    private static final byte[][] CACHE = new byte[6][];
+
+    static {
+        for (int fraudCount = 0; fraudCount <= 5; fraudCount++) {
+            float score = fraudCount / 5.0f;
+            boolean approved = score < 0.6f;
+            CACHE[fraudCount] = String.format(
+                    "{\"approved\":%b,\"fraud_score\":%.4f}",
+                    approved, score
+            ).getBytes(StandardCharsets.UTF_8);
+        }
+    }
+
+    public static byte[] get(int fraudCount) {
+        return CACHE[fraudCount];
+    }
+}
--- src/main/java/villani/dev/fraud/FraudCheckEndpoint.java
+++ src/main/java/villani/dev/fraud/FraudCheckEndpoint.java
@@ -4,7 +4,6 @@ import io.helidon.common.media.type.MediaTypes;
 import io.helidon.http.Http;
 import io.helidon.service.registry.Service;
 import io.helidon.webserver.http.RestServer;
-import io.helidon.webserver.http.ServerRequest;
 
 @SuppressWarnings("deprecation")
 @RestServer.Endpoint
@@ -21,12 +20,12 @@ public class FraudCheckEndpoint {
 
     @Http.POST
     @Http.Produces(MediaTypes.APPLICATION_JSON_VALUE)
-    public String checkScore(@Http.Entity String body) {
+    public byte[] checkScore(@Http.Entity String body) {
         //String body = req.content().as(String.class);
         float[] txArray = TransactionRequest.toRequestArray(body);
         //byte[] bodyBytes = req.content().as(byte[].class);
         //float[] txArray = TransactionRequestBytes.toRequestArray(bodyBytes);
-        return fraudCheckService.checkScore(txArray);
+        return fraudCheckService.checkScore2(txArray);
     }
 }
 
